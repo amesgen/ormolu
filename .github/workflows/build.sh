7fdffcfaa932fa4075d60043fc169e865efb3c33
amesgen <amesgen@amesgen.de>
2020-09-04 06:42:27 +0200

    add split-sections

--- .github/workflows/build.sh
+++ .github/workflows/build.sh
@@ -1,6 +1,6 @@
 set -e
 
 cabal update
-cabal build exe:ormolu --enable-executable-static --ghc-options=""
+cabal build exe:ormolu --enable-executable-static --ghc-options="-split-sections"
 
 cp $(find dist-newstyle -name ormolu -type f) .
