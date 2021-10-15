a14528168f536d5f29c5aee76daf92e2d048fe8d
amesgen <amesgen@amesgen.de>
2021-10-15 23:37:39 +0200

    Use macdylibbundler 1.0.0

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,12 +1,12 @@
 let
   macdylibbundler = self: super: {
     macdylibbundler = super.macdylibbundler.overrideAttrs (old: {
-      version = "20210804";
+      version = "1.0.0";
       src = super.fetchFromGitHub {
         owner = "auriamg";
         repo = "macdylibbundler";
-        rev = "5a6413cc4ea688ed59209b062f05aef092ee4585";
-        sha256 = "1wvfycdsysji5j4g3lr1dwk3hbnai8pibhajnfya9vy219clp281";
+        rev = "886b02b372e66e79ade5107aa0fdd359546c16b1";
+        sha256 = "02w04qvaf9v8yw8bgncx5qj3jx08xdfa855isvq92q27hsb8m8hv";
       };
     });
   };
