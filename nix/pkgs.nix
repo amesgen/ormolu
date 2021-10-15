34a7fd5e4a54280bb4e46327375b4229d86d931c
amesgen <amesgen@amesgen.de>
2021-10-15 18:39:00 +0200

    Fix build of macOS binaries

--- nix/pkgs.nix
+++ nix/pkgs.nix
@@ -1,5 +1,16 @@
 let
+  macdylibbundler = self: super: {
+    macdylibbundler = super.macdylibbundler.overrideAttrs (old: rec {
+      version = "1.0.0";
+      src = builtins.fetchTarball {
+        url = "https://github.com/auriamg/macdylibbundler/archive/refs/tags/${version}.tar.gz";
+        sha256 = "02w04qvaf9v8yw8bgncx5qj3jx08xdfa855isvq92q27hsb8m8hv";
+      };
+    });
+  };
   sources = import ./sources.nix { };
   haskellNix = import sources.haskellNix { };
-  pkgs = import haskellNix.sources.nixpkgs-unstable haskellNix.nixpkgsArgs;
-in pkgs
+  inherit (haskellNix) nixpkgsArgs;
+in
+import haskellNix.sources.nixpkgs-unstable
+  (nixpkgsArgs // { overlays = nixpkgsArgs.overlays ++ [ macdylibbundler ]; })
