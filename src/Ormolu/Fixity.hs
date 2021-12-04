2a58914bd4fb34f6da0b4cf8a6a659c2e353cccd
amesgen <amesgen@amesgen.de>
2021-12-04 21:22:37 +0100

    WIP: Parse fixities with TH

--- src/Ormolu/Fixity.hs
+++ src/Ormolu/Fixity.hs
@@ -1,12 +1,7 @@
-{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE RecordWildCards #-}
-{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
+{-# OPTIONS_GHC -Wno-unused-imports #-}
 
 -- | Definitons for fixity analysis.
 module Ormolu.Fixity
@@ -23,153 +18,29 @@ module Ormolu.Fixity
   )
 where
 
-import Data.Aeson (FromJSON (..), ToJSON (..), (.:), (.:?), (.=))
+import Control.Monad.IO.Class (MonadIO (liftIO))
 import qualified Data.Aeson as A
 import Data.ByteString (ByteString)
-import Data.FileEmbed (embedFile)
 import Data.Foldable (foldl')
-import Data.HashMap.Strict (HashMap)
-import qualified Data.HashMap.Strict as HashMap
-import Data.Hashable (Hashable (..))
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import qualified Data.List.NonEmpty as NE
+import Data.Map.Strict (Map)
+import qualified Data.Map.Strict as Map
 import Data.Maybe (fromJust, fromMaybe)
 import Data.Semigroup (sconcat)
 import qualified Data.Set as Set
 import qualified Data.Text as T
-import GHC.Generics (Generic)
-import GHC.Types.Fixity (FixityDirection (..))
+import qualified Language.Haskell.TH.Syntax as TH
+import Ormolu.Fixity.Types
 
--- | Fixity information about an infix operator that takes the uncertainty
--- that can arise from conflicting definitions into account.
-data FixityInfo = FixityInfo
-  { -- | Fixity direction if it is known
-    fiDirection :: Maybe FixityDirection,
-    -- | Minimum precedence level found in the (maybe conflicting)
-    -- definitions for the operator (inclusive)
-    fiMinPrecedence :: Int,
-    -- | Maximum precedence level found in the (maybe conflicting)
-    -- definitions for the operator (inclusive)
-    fiMaxPrecedence :: Int
-  }
-  deriving (Eq, Generic)
-
-instance FromJSON FixityInfo where
-  parseJSON = A.withObject "FixitiyInfo" $ \o ->
-    FixityInfo
-      <$> ( (o .:? "dir")
-              >>= maybe (pure Nothing) (fmap Just . parseFixityDirection)
-          )
-      <*> o .: "min_prec"
-      <*> o .: "max_prec"
-    where
-      parseFixityDirection = A.withText "FixityDirection" $ \case
-        "InfixL" -> pure InfixL
-        "InfixN" -> pure InfixN
-        "InfixR" -> pure InfixR
-        x -> fail (T.unpack x ++ " is not a fixity direction")
-
-instance ToJSON FixityInfo where
-  toJSON FixityInfo {..} =
-    A.object
-      [ "dir" .= (fixityDirectionToJSON <$> fiDirection),
-        "min_prec" .= fiMinPrecedence,
-        "max_prec" .= fiMaxPrecedence
-      ]
-    where
-      fixityDirectionToJSON x =
-        toJSON . T.pack . showFixityDirection $ x
-
-showFixityDirection :: FixityDirection -> String
-showFixityDirection = \case
-  InfixN -> "InfixN"
-  InfixR -> "InfixR"
-  InfixL -> "InfixL"
-
-instance Show FixityInfo where
-  show FixityInfo {..} =
-    "FixityInfo { "
-      <> "fiDirection = "
-      <> show (showFixityDirection <$> fiDirection)
-      <> ", fiMinPrecedence = "
-      <> show fiMinPrecedence
-      <> ", fiMaxPrecedence = "
-      <> show fiMaxPrecedence
-      <> " }"
-
--- TODO try to get rid of hashable and by extension of the orphans
-
-deriving instance Generic FixityDirection
-
-instance Hashable FixityDirection
-
-instance Hashable FixityInfo
-
--- | The lowest level of information we can have about an operator.
-defaultFixityInfo :: FixityInfo
-defaultFixityInfo =
-  FixityInfo
-    { fiDirection = Nothing,
-      fiMinPrecedence = 0,
-      fiMaxPrecedence = 9
-    }
-
--- | Fixity info of the built-in colon data constructor.
-colonFixityInfo :: FixityInfo
-colonFixityInfo =
-  FixityInfo
-    { fiDirection = Just InfixR,
-      fiMinPrecedence = 5,
-      fiMaxPrecedence = 5
-    }
-
--- | Gives the ability to merge two (maybe conflicting) definitions for an
--- operator, keeping the higher level of compatible information from both.
-instance Semigroup FixityInfo where
-  FixityInfo {fiDirection = dir1, fiMinPrecedence = min1, fiMaxPrecedence = max1}
-    <> FixityInfo {fiDirection = dir2, fiMinPrecedence = min2, fiMaxPrecedence = max2} =
-      FixityInfo
-        { fiDirection = dir',
-          fiMinPrecedence = min min1 min2,
-          fiMaxPrecedence = max max1 max2
-        }
-      where
-        dir' = case (dir1, dir2) of
-          (Just a, Just b) | a == b -> Just a
-          _ -> Nothing
-
--- | Map from the operator name to its 'FixityInfo'.
-type FixityMap = HashMap String FixityInfo
-
--- | The map of operators declared by each package and the popularity of
--- each package, if available.
-data HackageInfo
-  = HackageInfo
-      (HashMap String FixityMap)
-      (HashMap String Int)
-
-instance FromJSON HackageInfo where
-  parseJSON = A.withObject "HackageInfo" $ \o ->
-    HackageInfo
-      <$> o .: "operators"
-      <*> o .: "popularity"
-
-instance ToJSON HackageInfo where
-  toJSON (HackageInfo operators popularity) =
-    A.object
-      [ "operators" .= operators,
-        "popularity" .= popularity
-      ]
-
--- | Embed the contents of JSON file containing 'HackageInfo' in a
--- serialized form.
-hackageInfoFile :: ByteString
-hackageInfoFile = $(embedFile "extract-hackage-info/hackage-info.json")
-
-packageToOps :: HashMap String FixityMap
-packageToPopularity :: HashMap String Int
+packageToOps :: Map String FixityMap
+packageToPopularity :: Map String Int
 HackageInfo packageToOps packageToPopularity =
-  fromJust . A.decodeStrict $ hackageInfoFile
+  $$( do
+        let path = "extract-hackage-info/hackage-info.json"
+        info <- liftIO $ either fail pure =<< A.eitherDecodeFileStrict' path
+        TH.liftTyped info
+    )
 
 -- | The default value for the popularity ratio threshold, after which one
 -- very popular definition from packageToOps will completely rule out
@@ -204,9 +75,9 @@ buildFixityMap = buildFixityMap' packageToOps packageToPopularity
 -- used to build the final fixity map (op -> fixity).
 buildFixityMap' ::
   -- | Map package -> fixity map for operators defined in this package
-  HashMap String FixityMap ->
+  Map String FixityMap ->
   -- | Map package -> popularity
-  HashMap String Int ->
+  Map String Int ->
   -- | Explicitely known dependencies
   [String] ->
   -- | Popularity ratio threshold, after which a very popular package will
@@ -220,13 +91,13 @@ buildFixityMap'
   popularityMap
   cabalDependencies
   strategyThreshold =
-    -- HashMap.union is left biaised
-    HashMap.union baseFixityMap (HashMap.union cabalFixityMap hoogleFixityMap)
+    -- Map.union is left biaised
+    Map.union baseFixityMap (Map.union cabalFixityMap hoogleFixityMap)
     where
       baseFixityMap =
-        HashMap.insert ":" colonFixityInfo $
-          fromMaybe HashMap.empty $
-            HashMap.lookup "base" operatorMap
+        Map.insert ":" colonFixityInfo $
+          fromMaybe Map.empty $
+            Map.lookup "base" operatorMap
       cabalFixityMap =
         mergeFixityMaps
           popularityMap
@@ -238,11 +109,11 @@ buildFixityMap'
           strategyThreshold
           (buildPackageFixityMap <$> notCabalDependencies)
       notCabalDependencies =
-        HashMap.keys operatorMap `difference` cabalDependencies
+        Map.keys operatorMap `difference` cabalDependencies
       buildPackageFixityMap packageName =
         ( packageName,
-          fromMaybe HashMap.empty $
-            HashMap.lookup packageName operatorMap
+          fromMaybe Map.empty $
+            Map.lookup packageName operatorMap
         )
       difference l1 l2 =
         Set.toList $
@@ -254,7 +125,7 @@ buildFixityMap'
 -- strategies when conflicting definitions are encountered for an operator.
 mergeFixityMaps ::
   -- | Map package -> popularity
-  HashMap String Int ->
+  Map String Int ->
   -- | Popularity ratio threshold
   Float ->
   -- | List of (package name, package fixity map) to merge
@@ -262,23 +133,23 @@ mergeFixityMaps ::
   -- | Resulting fixity map
   FixityMap
 mergeFixityMaps popularityMap threshold packageMaps =
-  HashMap.map
-    (useThreshold threshold . NE.fromList . HashMap.toList)
+  Map.map
+    (useThreshold threshold . NE.fromList . Map.toList)
     scoredMap
   where
-    scoredMap = HashMap.map getScores opFixityMap
+    scoredMap = Map.map getScores opFixityMap
     -- when we encounter a duplicate key (op1) in the unionsWith operation,
     -- we have
     --   op1 -map-> {definitions1 -map-> originPackages}
     --   op1 -map-> {definitions2 -map-> originPackages}
     -- so we merge the keys (which have the type:
-    -- HashMap FixityInfo (NonEmpty String))
-    -- using 'HashMap.unionWith (<>)', to "concatenate" the list of
+    -- Map FixityInfo (NonEmpty String))
+    -- using 'Map.unionWith (<>)', to "concatenate" the list of
     -- definitions for this operator, and to also "concatenate" origin
     -- packages if a same definition is found in both maps
     opFixityMap =
-      unionsWith
-        (HashMap.unionWith (<>))
+      Map.unionsWith
+        (Map.unionWith (<>))
         (opFixityMapFrom <$> packageMaps)
     useThreshold ::
       -- Threshold
@@ -300,14 +171,14 @@ mergeFixityMaps popularityMap threshold packageMaps =
     getScores ::
       -- Map for a given operator associating each of its conflicting
       -- definitions with the packages that define it
-      HashMap FixityInfo (NonEmpty String) ->
+      Map FixityInfo (NonEmpty String) ->
       -- Map for a given operator associating each of its conflicting
       -- definitions with their score (= sum of the popularity of the
       -- packages that define it)
-      HashMap FixityInfo Int
+      Map FixityInfo Int
     getScores =
-      HashMap.map
-        (sum . fmap (fromMaybe 0 . flip HashMap.lookup popularityMap))
+      Map.map
+        (sum . fmap (fromMaybe 0 . flip Map.lookup popularityMap))
     opFixityMapFrom ::
       -- (packageName, package fixity map)
       (String, FixityMap) ->
@@ -316,10 +187,10 @@ mergeFixityMaps popularityMap threshold packageMaps =
       -- the list of packages that define it}
       -- (this list can only be == [packageName] in the context of this
       -- function)
-      HashMap String (HashMap FixityInfo (NonEmpty String))
+      Map String (Map FixityInfo (NonEmpty String))
     opFixityMapFrom (packageName, opsMap) =
-      HashMap.map
-        (flip HashMap.singleton (packageName :| []))
+      Map.map
+        (flip Map.singleton (packageName :| []))
         opsMap
     maxWith :: Ord b => (a -> b) -> NonEmpty a -> NonEmpty a
     maxWith f xs = snd $ foldl' comp (f h, h :| []) t
@@ -331,12 +202,3 @@ mergeFixityMaps popularityMap threshold packageMaps =
                   | fMax < fX -> (fX, x :| [])
                   | fMax == fX -> (fMax, NE.cons x maxs)
                   | otherwise -> (fMax, maxs)
-    -- Same as Map.unionsWith, but for HashMap
-    unionsWith ::
-      (Eq k, Hashable k) =>
-      (v -> v -> v) ->
-      [HashMap k v] ->
-      HashMap k v
-    unionsWith f = \case
-      [] -> HashMap.empty
-      m : ms -> foldl' (HashMap.unionWith f) m ms
