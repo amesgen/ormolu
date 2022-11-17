f0882d4fc1b9933ae53299ce0671989a2ddbfe97
Alexander Esgen <alexander.esgen@tweag.io>
2022-11-17 19:25:08 +0100

    Flakeify

---
+++ flake.nix
@@ -0,0 +1,153 @@
+{
+  inputs = {
+    haskellNix.url = "github:input-output-hk/haskell.nix";
+    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    flake-utils.follows = "haskellNix/flake-utils";
+    pre-commit-hooks = {
+      url = "github:cachix/pre-commit-hooks.nix";
+      inputs.nixpkgs.follows = "nixpkgs";
+      inputs.flake-utils.follows = "flake-utils";
+    };
+  };
+  outputs = inputs@{ self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          inherit (inputs.haskellNix) config;
+          overlays = [ inputs.haskellNix.overlay ];
+        };
+        inherit (pkgs) lib haskell-nix;
+        inherit (haskell-nix) haskellLib;
+
+        defaultGHCVersion = "ghc925";
+        ghcVersions = [ "ghc902" defaultGHCVersion ];
+        perGHC = lib.genAttrs ghcVersions (ghcVersion:
+          let
+            hsPkgs = pkgs.haskell-nix.cabalProject {
+              src = ./.;
+              compiler-nix-name = ghcVersion;
+              modules = [{
+                packages.ormolu.writeHieFiles = true;
+                packages.ormolu.components.exes.ormolu.preBuild =
+                  lib.mkIf (self ? rev) ''export ORMOLU_REV=${self.rev}'';
+              }];
+            };
+            inherit (hsPkgs.ormolu.components.exes) ormolu;
+            hackageTests = import ./expected-failures { inherit pkgs ormolu; };
+            regionTests = import ./region-tests { inherit pkgs ormolu; };
+            fixityTests = import ./fixity-tests { inherit pkgs ormolu; };
+            packages = lib.recurseIntoAttrs ({
+              inherit ormolu;
+              ormoluTests = haskellLib.collectChecks' hsPkgs;
+              dev = { inherit hsPkgs; };
+            } // hackageTests // regionTests // fixityTests
+            // lib.optionalAttrs (ghcVersion == defaultGHCVersion) {
+              inherit (hsPkgs.extract-hackage-info.components.exes) extract-hackage-info;
+              weeder = pkgs.runCommand
+                "ormolu-weeder"
+                {
+                  buildInputs = [ (hsPkgs.tool "weeder" "2.4.0") ];
+                } ''
+                mkdir -p $out
+                export XDG_CACHE_HOME=$TMPDIR/cache
+                weeder --config ${./weeder.dhall} \
+                  --hie-directory ${hsPkgs.ormolu.components.library.hie} \
+                  --hie-directory ${hsPkgs.ormolu.components.exes.ormolu.hie} \
+                  --hie-directory ${hsPkgs.ormolu.components.tests.tests.hie}
+              '';
+            });
+          in
+          packages // {
+            ci = pkgs.linkFarmFromDrvs "ormolu-ci-${ghcVersion}"
+              (lib.attrValues (flake-utils.lib.flattenTree packages));
+          });
+
+        binaries =
+          let
+            hsPkgs = perGHC.${defaultGHCVersion}.dev.hsPkgs.appendModule {
+              modules = [{
+                dontStrip = false;
+                dontPatchELF = false;
+                enableDeadCodeElimination = true;
+              }];
+            };
+            ormoluExe = hsPkgs: hsPkgs.hsPkgs.ormolu.components.exes.ormolu;
+          in
+          lib.recurseIntoAttrs {
+            Linux = ormoluExe hsPkgs.projectCross.musl64;
+            macOS = pkgs.runCommand "ormolu-macOS"
+              {
+                nativeBuildInputs = [ pkgs.macdylibbundler ];
+              } ''
+              mkdir -p $out/bin
+              cp ${ormoluExe hsPkgs}/bin/ormolu $out/bin/ormolu
+              chmod 755 $out/bin/ormolu
+              dylibbundler -b \
+                -x $out/bin/ormolu \
+                -d $out/bin \
+                -p '@executable_path'
+            '';
+            Windows = ormoluExe hsPkgs.projectCross.mingwW64;
+          };
+      in
+      {
+        packages = flake-utils.lib.flattenTree (perGHC // {
+          inherit binaries;
+          default = perGHC.${defaultGHCVersion}.ormolu;
+          pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
+            src = ./.;
+            hooks = {
+              nixpkgs-fmt.enable = true;
+              deadnix.enable = true;
+            };
+          };
+        });
+        apps = {
+          default = flake-utils.lib.mkApp {
+            drv = perGHC.${defaultGHCVersion}.ormolu;
+            exePath = "/bin/ormolu";
+          };
+          extract-hackage-info = flake-utils.lib.mkApp {
+            drv = perGHC.${defaultGHCVersion}.extract-hackage-info;
+            exePath = "/bin/extract-hackage-info";
+          };
+          format = flake-utils.lib.mkApp {
+            drv = pkgs.writeShellApplication {
+              name = "ormolu-format";
+              text = builtins.readFile ./format.sh;
+              runtimeInputs = [
+                (perGHC.${defaultGHCVersion}.dev.hsPkgs.tool "cabal" "latest")
+                self.packages.${system}.default
+              ];
+            };
+          };
+        };
+        devShells =
+          let
+            shellFor = packages: perGHC.${defaultGHCVersion}.dev.hsPkgs.shellFor {
+              inherit packages;
+              tools = {
+                cabal = "latest";
+                haskell-language-server = "latest";
+              };
+              withHoogle = false;
+              exactDeps = false;
+              inherit (self.packages.${system}.pre-commit-check) shellHook;
+            };
+          in
+          {
+            default = shellFor (ps: [ ps.ormolu ]);
+            extractHackageInfo = shellFor (ps: [ ps.extract-hackage-info ]);
+          };
+        legacyPackages = perGHC.${defaultGHCVersion};
+      });
+  nixConfig = {
+    extra-substituters = [
+      "https://cache.iog.io"
+    ];
+    extra-trusted-public-keys = [
+      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
+    ];
+  };
+}
--- format.sh
+++ format.sh
@@ -1,18 +1,10 @@
-#!/usr/bin/env nix-shell
-#!nix-shell default.nix -A dev.cabalAndOrmolu -i bash --pure
-#
 # Format Ormolu using current version of Ormolu.
-
-set -e
 
 export LANG="C.UTF-8"
 
 cabal format
-pushd extract-hackage-info && cabal format && popd
-pushd ormolu-live && cabal format && popd
+(cd extract-hackage-info && cabal format)
 
-ormolu -m inplace $(find app -type f -name "*.hs")
-ormolu -m inplace $(find src -type f -name "*.hs-boot" -o -name "*.hs")
-ormolu -m inplace $(find tests -type f -name "*.hs")
-ormolu -m inplace $(find ormolu-live/src -type f -name "*.hs")
-ormolu -m inplace $(find extract-hackage-info/src -type f -name "*.hs")
+export dirs="src app tests extract-hackage-info/src"
+# shellcheck disable=SC2046,SC2086
+ormolu -m inplace $(find $dirs -type f -name "*.hs" -o -name "*.hs-boot")
