c80e5ef277a4bfd7ea14e09b35afaaf7590a96d7
Alexander Esgen <alexander.esgen@tweag.io>
2023-01-04 21:04:58 +0100

    Allow to disable bundling fixities
    Should be reverted once the WASM backend supports TH.

--- src/Ormolu/Fixity.hs
+++ src/Ormolu/Fixity.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE CPP #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiWayIf #-}
 {-# LANGUAGE TemplateHaskell #-}
@@ -23,7 +24,6 @@ where
 import qualified Data.Binary as Binary
 import qualified Data.Binary.Get as Binary
 import qualified Data.ByteString.Lazy as BL
-import Data.FileEmbed (embedFile)
 import Data.Foldable (foldl')
 import Data.List.NonEmpty (NonEmpty ((:|)))
 import qualified Data.List.NonEmpty as NE
@@ -35,12 +35,29 @@ import Data.Semigroup (sconcat)
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Ormolu.Fixity.Internal
+#if BUNDLE_FIXITIES
+import Data.FileEmbed (embedFile)
+#else
+import qualified Data.ByteString.Unsafe as BU
+import Foreign.Ptr
+import System.Environment (getEnv)
+import System.IO.Unsafe (unsafePerformIO)
+#endif
 
 packageToOps :: Map String FixityMap
 packageToPopularity :: Map String Int
+#if BUNDLE_FIXITIES
 HackageInfo packageToOps packageToPopularity =
   Binary.runGet Binary.get $
     BL.fromStrict $(embedFile "extract-hackage-info/hackage-info.bin")
+#else
+HackageInfo packageToOps packageToPopularity = unsafePerformIO $ do
+  (ptr, len) <- read <$> getEnv "ORMOLU_HACKAGE_INFO"
+  Binary.runGet Binary.get . BL.fromStrict
+    <$> BU.unsafePackMallocCStringLen (intPtrToPtr $ IntPtr ptr, len)
+{-# NOINLINE packageToOps #-}
+{-# NOINLINE packageToPopularity #-}
+#endif
 
 -- | List of packages shipped with GHC, for which the download count from
 -- Hackage does not reflect their high popularity.
